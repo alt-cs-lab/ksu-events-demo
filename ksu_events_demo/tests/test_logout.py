8b744ec92ba170424c74569c6913bcc35275627c
Whipple <94202163+g0ggle@users.noreply.github.com>
2024-10-16 10:40:12 -0500

    added simple single logout test

---
+++ ksu_events_demo/tests/test_logout.py
@@ -0,0 +1,22 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+class CasLoginTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpass')
+        self.logout_url = reverse('cas_ng_logout')
+    
+    def test_cas_logout(self):
+        # Simulate the CAS login process
+        self.client.login(username='testuser', password='testpass')
+
+        response = self.client.get(self.logout_url) 
+
+        # Check if the response is a redirect, which means the logout was successful
+        self.assertEqual(response.status_code, 302)
+
+        # Verify that the user is authenticated
+        self.assertFalse(self.user in self.client.session)
+
