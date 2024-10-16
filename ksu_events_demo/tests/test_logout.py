7cc21eb5565c935f7f25bfdc63a9b4fe652c280f
Whipple <94202163+g0ggle@users.noreply.github.com>
2024-10-16 11:08:45 -0500

    added second logout test

--- ksu_events_demo/tests/test_logout.py
+++ ksu_events_demo/tests/test_logout.py
@@ -8,7 +8,7 @@ class CasLoginTestCase(TestCase):
         self.user = get_user_model().objects.create_user(username='testuser', password='testpass')
         self.logout_url = reverse('cas_ng_logout')
     
-    def test_cas_logout(self):
+    def test_cas_logout1(self):
         # Simulate the CAS login process
         self.client.login(username='testuser', password='testpass')
 
@@ -17,6 +17,17 @@ class CasLoginTestCase(TestCase):
         # Check if the response is a redirect, which means the logout was successful
         self.assertEqual(response.status_code, 302)
 
-        # Verify that the user is authenticated
+        # Verify that the user is current session
         self.assertFalse(self.user in self.client.session)
 
+    def test_cas_logout2(self):
+        # Simulate the CAS login process
+        self.client.login(username='badUser', password='BadPass')
+        self.assertFalse(self.client.login(username='badUser', password='BadPass'), "Login should fail with incorrect credentials")
+
+        response = self.client.get(self.logout_url) 
+
+        #self.assertNotEqual(response.status_code, 302)
+
+        # Verify that the user is authenticated
+        self.assertFalse(self.user in self.client.session)
