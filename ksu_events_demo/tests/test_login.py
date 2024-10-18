db62b4d40ae98bd8f2e51f059e752e8e499c6252
Whipple <94202163+g0ggle@users.noreply.github.com>
2024-10-18 11:20:51 -0500

    added some extra tests

--- ksu_events_demo/tests/test_login.py
+++ ksu_events_demo/tests/test_login.py
@@ -7,14 +7,20 @@ class CasLoginTestCase(TestCase):
         self.client = Client()
         self.user = get_user_model().objects.create_user(username='testuser', password='testpass')
         self.login_url = reverse('cas_ng_login')
+        self.protected_url = reverse('authed')
     
-    def test_cas_login(self):
+    def test_cas_login1(self):
         # Simulate the CAS login process
-        response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'testpass'})
+        self.client.login(username='testuser', password='password')
+        response = self.client.get(self.protected_url, follow=True)
         
-        # Check if the response is a redirect, which means the login was successful
-        self.assertEqual(response.status_code, 302)
-
         # Verify that the user is authenticated
         self.assertTrue(self.user.is_authenticated)
 
+    def test_cas_login2(self):
+        #BadLogin = self.client.login(username='badUser', password='BadPass')
+        #self.assertFalse(BadLogin, "Login should fail with incorrect credentials")
+
+        response = self.client.get(self.protected_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.headers.get('Location'), "/accounts/login/?next=/authed/")
