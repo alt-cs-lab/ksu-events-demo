44d0e5a829d490311a87682abf1e608608b7160e
JKenslow <jkenslow@ksu.edu>
2024-11-16 10:12:43 -0600

    removed conflicting admin url paths

--- ksu_events_demo/urls.py
+++ ksu_events_demo/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from ksu_events.ksu_events import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('ksu_events.urls')),
     path('accounts/', include('ksu_events.ksu_cas_auth.urls')),
     path('authed/', views.redirect, name='authed')
