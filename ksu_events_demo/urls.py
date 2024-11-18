a7404a1e0df789c6e1a73558e9daf3ebf26d773e
Ia-in03 <65005143+Ia-in03@users.noreply.github.com>
2024-11-18 09:05:42 +0000

    ready for main sprint PR

--- ksu_events_demo/urls.py
+++ ksu_events_demo/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from ksu_events.ksu_events import views
+from ksu_events import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
