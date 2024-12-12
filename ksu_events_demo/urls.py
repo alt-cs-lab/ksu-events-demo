e7eeded4842cabb6d9f615efa96f8783e50d5d7f
Whipple <94202163+g0ggle@users.noreply.github.com>
2024-12-11 21:25:18 -0600

    working changes to the project made

--- ksu_events_demo/urls.py
+++ ksu_events_demo/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from ksu_events import views
 
+'''Home page comes from ksu_events'''
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ksu_events.urls')),
